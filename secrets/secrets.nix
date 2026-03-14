28fdcb17b982f0ea178270885105e1483c5fcbc2
Sergei Razgulin <sergei.razgulin@gmail.com>
2026-03-14 17:32:27 -0600

    Adding pi-worker 3 and 4

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -7,7 +7,8 @@ let
     "pi-master-1" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMy5MgoFs0BgZAwJWbUlOpkFrzlvZAzVOZMl8gan5JJh root@pi-master-1";
     "pi-worker-1" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6ujLebyeMT2iipj/PysxQrR5uxCrLwLptsW5fkX491 root@pi-worker-1";
     "pi-worker-2" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDdp/ByOrM644lD1gxncjAFczEkR5KE150bdT+okq/si root@pi-worker-2";
-#    "pi-worker-3" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI... w3 ...";
+    "pi-worker-3" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEXgMzm999HbggNNHOXYhmki8sAnTnIptRQnv67hOF6K root@pi-worker-3";
+    "pi-worker-4" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRk5gK9+6Z30wlnYSBZbZwPK+goPkvyI5gK49H4/6We root@pi-worker-4";
     "r630-storage" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMiY9RtgVBVwjWIBknwGDxK38Vd5E15D/xSsyML36biO root@r630-storage";
   };
 
